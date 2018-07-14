430bd5a38224e19add2033cacc4a74862512ae7e
Saad <saadbennmushtaq@gmail.com>
2018-07-14 15:02:21 -0500

    Add header files

--- fat_fs.h
+++ fat_fs.h
@@ -1,70 +1,61 @@
-#ifndef FAT_FS_H
-#define FAT_FS_H
+#pragma pack(push)
+#pragma pack(1)
 
-#include <inttypes.h>
+// For reading files in our file system
+typedef struct Dir_Info {
+    uint8_t DIR_Name[11];
+    uint8_t DIR_Attr;
+    uint8_t DIR_NTRes;
+    uint8_t DIR_CrtTimeTenth;
+    uint16_t DIR_CrtTime;
+    uint16_t DIR_CrtDate;
+    uint16_t DIR_LstAccDate;
+    uint16_t DIR_FstClusHI;
+    uint16_t DIR_WrtTime;
+    uint16_t DIR_WrtDate;
+    uint16_t DIR_FstClusLO;
+    uint32_t DIR_FileSize;
+} Dir_Info;
 
-#define DIR_Name_LENGTH 11
-#define CMD_INFO "INFO"
-#define FName_LENGTH (DIR_Name_LENGTH + 1)
-#define RootName_LENGTH 8
-#define MAX_TIMESTAMP_STR 20
-#define EPOCH 1980
-#define ATTR_LONG_NAME_MASK (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID | ATTR_DIRECTORY | ATTR_ARCHIVE)
-
-#define FIRST_DATA_CLUSTER 2
-
-#define MEDIA_FIXED 0xF8
-#define MEDIA_REMOVABLE 0xF0
-#define DRV_NUM_FLOP 0x00
-#define DRV_NUM_HD 0x80
-
-#define ATTR_READ_ONLY 0x01
-#define ATTR_HIDDEN 0x02
-#define ATTR_SYSTEM 0x04
-#define ATTR_VOLUME_ID 0x08
-#define ATTR_DIRECTORY 0x10
-#define ATTR_ARCHIVE 0x20
-#define ATTR_LONG_NAME (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID)
+#pragma pack(pop)
 
 
-typedef enum {
-	FS_FAT12,
-	FS_FAT16,
-	FS_FAT32
-} FS_Type;
+// Structure for storing file info
+#pragma pack(push)
+#pragma pack(1)
 
-struct FS_Instance_struct {
-  char *image_path;
-  uint8_t *image;
+typedef struct FS_Info {
+    uint32_t FSI_LeadSig;
+    char FSI_Rerserved1[480];
+    uint32_t FSI_StrucSig;
+    uint32_t FSI_Free_Count;
+    uint32_t FSI_Nxt_Free;
+    char FSI_Reserved2[12];
+    uint32_t FSI_TrailSig;
+} FS_Info;
 
-	FS_Type fs_type;
-	
-	uint32_t RootDirSectors;
-	uint32_t FirstDataSector;
-	uint32_t DataSec;
-	
-	char volume_id[DIR_Name_LENGTH + 1];
-};
+#pragma pack(pop)
 
-struct fatEntry_struct {
-		uint8_t DIR_Name[DIR_Name_LENGTH];
-		uint8_t DIR_Attr;
-		uint8_t DIR_NTRes;
-		uint8_t DIR_CrtTimeTenth;
-		uint16_t DIR_FstClusHI;
-		uint16_t DIR_FstClusLO;
-		uint32_t DIR_FileSize;
-};
 
-typedef struct FS_Instance_struct FS_Instance;
-typedef uint32_t FS_CurrentDir;
-typedef struct fatEntry_struct fatEntry;
+void ReadDiectories(uint32_t);
 
-FS_Instance *fs_create_instance(char *image_path);
-FS_CurrentDir fs_get_root(FS_Instance *fat_fs);
+// printing functions
+void PrintRootDirectory(uint32_t);
+int print_dir_recursively(fat32BS *, uint32_t, uint32_t);
+void print_info(fat32BS *);
+void print_contents_of_the_file(fat32BS *, uint32_t, char *);
+void print_dash(int);
 
-void print_info(FS_Instance *fat_fs);
+uint32_t getClusterNumber(uint16_t, uint16_t);
 
-void fs_cleanup(FS_Instance *fat_fs);
+// validate the directory
+bool is_directory(Dir_Info *currDir);
 
-#endif
+uint32_t check_chain(fat32BS *, uint32_t);
+void getFileContent(fat32BS *bpb, uint32_t);
+char *GetNextFolderToPath();
+uint32_t get_sector_of_cluster(fat32BS *, uint32_t);
+
+char *tokenize_dir_file_name(char *);
+
+char *seek_and_read_file_contents(uint32_t, uint32_t);
